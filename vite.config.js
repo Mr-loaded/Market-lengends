9e8967a96a1832950f6bbfd0099490bd58fdec40
Mr-loaded <josepharira@gmail.com>
2026-05-22 08:47:04 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',
   build: {
     outDir: 'dist',
     sourcemap: false,
     minify: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-        }
-      }
-    }
   }
 })
